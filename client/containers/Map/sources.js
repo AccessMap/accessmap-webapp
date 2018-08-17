292c6755d9ce62ab7f60ad17a213fec21c3cac5d
= <=>
2018-08-17 15:22:24 -0700

    Simplified host settings

--- client/containers/Map/sources.js
+++ client/containers/Map/sources.js
@@ -2,16 +2,13 @@ import React from 'react';
 
 import { Source } from 'react-mapbox-gl';
 
-const protocol = process.env.HOST ? 'https://' : 'http://';
-const host = process.env.HOST || 'localhost:2015';
-
 const Sources = () => (
   <React.Fragment>
     <Source
       id='pedestrian'
       tileJsonSource={{
         type: 'vector',
-        url: `${protocol}${host}/tiles/tilejson/pedestrian.json`,
+        url: '/tiles/tilejson/pedestrian.json',
       }}
     />
   </React.Fragment>
