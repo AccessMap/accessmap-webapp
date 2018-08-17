a53c2d16d0e6598dd0520da64ef2308ea99092b5
= <=>
2018-08-17 13:57:02 -0700

    Added woff font icons and Intl polyfill for iOS 9 Safari compatibility

--- client/polyfills.js
+++ client/polyfills.js
@@ -6,3 +6,10 @@ import 'whatwg-fetch';
 
 // Adds Object.values, etc. Auto-calls its shim method.
 import 'es7-shim/browser';
+
+// Adds Intl if it doesn't exist (e.g. iOS 9 Safari). Needed for time / date pickers
+if (!global.Intl) {
+  // Note: Have to use require() for conditional import
+  require('intl'); // eslint-disable-line global-require
+  require('intl/locale-data/jsonp/en.js'); // eslint-disable-line global-require
+}
