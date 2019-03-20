407af88a19cf1544675d840eb6892f6f0eb22d37
= <=>
2019-03-19 18:09:11 -0700

    Switched to ugliyjs plugin for webpack to make rakam silent in production builds

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,12 +1,22 @@
 const webpack = require("webpack");
 const merge = require("webpack-merge");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
   mode: "production",
   optimization: {
-    minimize: true
+    minimize: true,
+    minimizer: [
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          compress: {
+            drop_console: true
+          }
+        }
+      })
+    ]
   },
   plugins: [
     new webpack.DefinePlugin({
