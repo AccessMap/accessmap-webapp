b25835fbc2cd3bb7232b6ecdde715cd0d8e3161d
= <=>
2019-03-22 22:22:49 -0700

    Added more helpful messages when routes fail

--- src/reducers/toasts.js
+++ src/reducers/toasts.js
@@ -34,31 +34,15 @@ export default (state = defaults, action) => {
       toasts.push("Failed to save profile. Try again or contact us.");
       return toasts;
     case RECEIVE_ROUTE:
-      switch (action.payload.routeResult.code) {
-        case "GraphNotReady":
-        case "SpatialIndexNotReady":
-          text =
-            "Server is rebuilding the graph, please wait a few seconds and try again.";
-          break;
-        case "BothFarAway":
-          text = "Start point is too far from any valid footpaths.";
-          break;
-        case "OriginFarAway":
-          text = "End point is too far from any valid footpaths.";
-          break;
-        case "DestinationFarAway":
-          text =
-            "Both the start and ends points are too far from any valid footpaths.";
-          break;
-        case "NoRoute":
-          text =
-            "No path between those points. If you think this was in error, try placing the waypoint closer to a colored/filled-in footpath.";
-          break;
-        default:
-          break;
-      }
       switch (action.payload.routeResult.status) {
-        case "Failed":
+        case "InvalidWaypoint":
+          if (action.payload.routeResult.status_data.index === 0) {
+            text = "No usable footway near origin.";
+          } else {
+            text = "No usable footway near destination.";
+          }
+          break;
+        case "NoPath":
           text = "No path found between those points.";
           break;
         default:
