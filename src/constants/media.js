49b79fa58da1d3ff2e2720a14f185c9df8509875
= <=>
2019-08-02 18:52:47 -0700

    Fixed inconsistencies in detecting device type and orientation

--- src/constants/media.js
+++ src/constants/media.js
@@ -1,7 +1,7 @@
 // Internally used responsive media breakpoints
 
-const MOBILE_MIN_WIDTH = 320;
-const TABLET_MIN_WIDTH = 820;
+const MOBILE_MIN_WIDTH = 360;
+const TABLET_MIN_WIDTH = 824;
 const DESKTOP_MIN_WIDTH = 1024;
 
 export { MOBILE_MIN_WIDTH, TABLET_MIN_WIDTH, DESKTOP_MIN_WIDTH };
