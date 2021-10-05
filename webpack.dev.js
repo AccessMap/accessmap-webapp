355a73af5e6d30369e5e15b280b2464dbfa875e8
= <=>
2021-10-05 10:30:18 -0700

    Reverted to older-style tile endpoints

--- webpack.dev.js
+++ webpack.dev.js
@@ -36,6 +36,12 @@ module.exports = merge(common, {
       disableDotRule: true,
     },
     port: 3000,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers":
+        "X-Requested-With, content-type, Authorization",
+    },
     open: false,
     compress: false,
     inline: true,
